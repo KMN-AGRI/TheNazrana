7b7d2e55b5874a461189db71ffdd255ed0ecbb2e
rdlogout <logoutrd@hotmail.com>
2022-06-02 12:31:25 +0530

    account updates

--- BackendApi/Controllers/AccountController.cs
+++ BackendApi/Controllers/AccountController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using SharedModel.Clients.MainSite;
+using SharedModel.Clients.Shared;
 using SharedModel.Contexts;
+using SharedModel.Helpers;
 using SharedModel.Repository;
 
 namespace BackendApi.Controllers
@@ -16,10 +19,95 @@ namespace BackendApi.Controllers
 			this.userRepository = userRepository;
 		}
 
+		[HttpGet]
+		public IActionResult Index()
+			=> Ok(context.AppUsers
+				.Select(s => new
+				{
+					s.Id,
+					s.Name,
+					s.UserName,
+					s.Email,
+					s.DOB,
+					s.ImageUri,
+					s.Date,
+
+				}).SingleOrDefault(s => s.Id == userRepository.Id()));
+		[HttpPost]
+		public async Task<IActionResult> updateUser([FromForm]UserProfile update)
+		{
+			var user = await context
+				.AppUsers
+				.FindAsync(userRepository.Id());
+
+			user.Name = update.Name;
+			user.DOB = update.DOB;
+			context.AppUsers.Update(user);
+			await context.SaveChangesAsync();
+			return Ok(new ApiResponse("Profile Update Successfully", true, new
+			{
+				user.Id,
+				user.Name,
+				user.UserName,
+				user.Email,
+				user.DOB,
+				user.ImageUri,
+				user.Date,
+			}));
+		}
+
 		[HttpGet("orders")]
 		public IActionResult Orders()
 			=> Ok(context.Orders
+				.Select(s=>new
+				{
+					s.Id,
+					s.User,
+					s.Address,
+					s.Total,
+					s.SubTotal,
+					s.Discount,
+					s.Status,
+					Items=s.Items
+					.Select(i=>new
+					{
+						Product=new
+						{
+							i.Product.Title,
+							image = i.Product.Medias.Select(k => Settings.imageKitUrl + k.ServerName).FirstOrDefault()
+						},
+						i.Amount,
+						i.Quantity,
+						i.Feedback
+					})
+				})
 				.Where(s => s.User == userRepository.Id()));
+
+		[HttpGet("/Track/{id}")]
+		public IActionResult trackOrder(string id)
+			=> Ok(context.Orders.Select(s => new
+			{
+				s.Id,
+				s.User,
+				s.Address,
+				s.Total,
+				s.SubTotal,
+				s.Discount,
+				s.Status,
+				s.Events,
+				Items = s.Items
+					 .Select(i => new
+					 {
+						 Product = new
+						 {
+							 i.Product.Title,
+							 image = i.Product.Medias.Select(k => Settings.imageKitUrl + k.ServerName).FirstOrDefault()
+						 },
+						 i.Amount,
+						 i.Quantity,
+						 i.Feedback
+					 }),
+			}).SingleOrDefault(s=>s.Id==id));
 
 	}
 }
---
+++ SharedModel/Clients/MainSite/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace SharedModel.Clients.MainSite
+{
+	public class UserProfile
+	{
+		public IFormFile Image { get; set; }
+		public string Name { get; set; }
+		public DateTime? DOB { get; set; }
+	}
+}
+
